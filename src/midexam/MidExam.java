3e24c7ccaf456f9085298104e6e17a76f1a280f6
mehtab <mehtab@Mehtab-Workstation>
2021-05-01 15:18:42 +0500

    Mid First commit

---
+++ src/midexam/MidExam.java
@@ -0,0 +1,16 @@
+package midexam;
+
+/**
+ *
+ * @author mehtab
+ */
+public class MidExam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
